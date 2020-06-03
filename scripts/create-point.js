183bb225f002ce903ce2614b2fec159e9ff2ee76
NayraCruz <nayra.ncruz@gmail.com>
2020-06-03 15:14:04 -0300

    Continuação dos scripts do formulário | Correção de bug ao escolher a cidade após mudar os estados | Implementação parcial do script para os itens de coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -23,11 +23,14 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = '<option value selected>Selecione a cidade</option>';
+    citySelect.disabled = true;
+
     fetch(url)
         .then(res => res.json())
         .then(cities => {
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
             }
 
             citySelect.disabled = false;
@@ -37,3 +40,37 @@ function getCities(event) {
 document
     .querySelector('#uf')
     .addEventListener('change', getCities);
+
+/* 
+    Itens de coleta 
+*/
+// Pegar todos os .collect-item
+const itemsToCollect = document.querySelectorAll(".collect-item");
+
+// Percorre todos os itemsToCollect
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem);
+}
+
+function handleSelectedItem(event) {
+    const item = event.target;
+
+    // Adiciona ou remove uma classe
+    item.classList.toggle('-selected');
+    
+    // Procura o checkbox
+    const checkboxItem = event.target.querySelector('.collect-item__checkbox');
+    
+    // Marca ou desmarca o checkbox dependendo se tem a classe .-selected
+    if (item.classList.contains('-selected')) {
+        checkboxItem.checked = true;
+    } else {
+        checkboxItem.checked = false;
+    }
+    // console.log(checkboxItem.checked); // Testa o checkbox
+
+    
+    
+
+    const itemId = item.dataset.id;
+}
